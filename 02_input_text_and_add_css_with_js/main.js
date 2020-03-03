3397bfb49cf512583947fdd4acb435cbdb35b890
CommonMatter <hoffmelanie@gmail.com>
2020-03-02 20:21:32 -0500

    edited 02

--- 02_input_text_and_add_css_with_js/main.js
+++ 02_input_text_and_add_css_with_js/main.js
@@ -10,3 +10,13 @@ function use_name() {
         $("#welcome_img").css("display", "block");
     }
 }
+
+
+function revert() {
+  $("#input_name").val("");
+  $("#welcome").html("This little corner of the web was made just for you.");
+  $("#welcome_img").css("display", "none");
+  $("#puppy").css("display", "none");
+  $("#welcome").css("display", "block");
+  $("#pre_name").css("display", "block");
+}
